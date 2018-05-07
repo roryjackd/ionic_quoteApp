cf32f2526578bde0cf86db0d8694debf6257cafb
roryjackd <roryjackd@yahoo.com>
2018-05-07 13:31:07 -0600

    Added alerts

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlertController } from "ionic-angular";
 import { NavParams } from "ionic-angular";
 import { Quote } from "../../data/quote/quote.interface";
 
@@ -10,7 +11,9 @@ import { Quote } from "../../data/quote/quote.interface";
 export class QuotesPage {
   quoteGroup: {category: string, quotes: Quote[], icon: string};
 
- constructor(private navParams: NavParams) {}
+ constructor(
+   private navParams: NavParams,
+   private alertCtrl: AlertController) {}
 
  ngOnInit() {
    this.quoteGroup = this.navParams.data;
@@ -19,4 +22,29 @@ export class QuotesPage {
  // ionViewDidLoad(){
  //   this.quoteGroup = this.navParams.data;
  // }
+
+ onAddToFavorite(selectedQuote: Quote){
+   const alert = this.alertCtrl.create({
+     title: 'Add Quote',
+     subtitle: 'Are you sure?',
+     message: 'Are you sure you want to add the quote?',
+     buttons: [
+       {
+         text: 'Yes, go ahead',
+         handler: () => {
+           console.log('Ok');
+         }
+       },
+       {
+         text: 'No, I changed my mind!',
+         role: 'cancel',
+         handler: () => {
+           console.log('Cancelled!');
+         }
+       }
+     ]
+   });
+
+   alert.present();
+ }
 }
