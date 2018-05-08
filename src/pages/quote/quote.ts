9717cc5e7d5605ffc107aef7c6b5efb9e505535e
roryjackd <roryjackd@yahoo.com>
2018-05-08 17:14:37 -0600

    Set up dismissal of modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ViewController } from "ionic-angular";
 
-@IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+ constructor (private viewCtrl: ViewController) {} 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
-  }
-
+ onClose() {
+   this.viewCtrl.dismiss();
+ }
 }
