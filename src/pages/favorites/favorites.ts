7d4fc7e7dc49178ac223bb7d9927e553eabb2044
roryjackd <roryjackd@yahoo.com>
2018-05-08 21:28:16 -0600

    Passing data from a modal and back

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -23,5 +23,8 @@ export class FavoritesPage {
   onViewQuote(quote: Quote) {
     const modal = this.modalCtrl.create(QuotePage, quote);
     modal.present();
+    modal.onDidDismiss((remove: boolean) => {
+
+    });
   }
 }
