a55fc9dfc60a1bca8b7ed71e1efd00d2a35d8f3c
roryjackd <roryjackd@yahoo.com>
2018-05-11 16:48:45 -0600

    Added alternative item background

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -5,6 +5,7 @@ import { ModalController } from 'ionic-angular';
 import { Quote } from "../../data/quote.interface";
 import { QuotesService } from "../../services/quotes";
 import { QuotePage } from "../quote/quote";
+import { SettingsService } from "../../services/settings";
 
 @Component({
   selector: 'page-favorites',
@@ -14,7 +15,8 @@ export class FavoritesPage {
   quotes: Quote[];
 
   constructor (private quotesService: QuotesService,
-               private modalCtrl: ModalController) {}
+               private modalCtrl: ModalController,
+               private settingsService: SettingsService) {}
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
@@ -37,5 +39,13 @@ export class FavoritesPage {
       return quoteEl.id == quote.id;
     });
     this.quotes.splice(position, 1);
+  }
+
+  getBackground() {
+    return this.settingsService.isAltBackground() ? 'altQuoteBackground' : 'quoteBackground';
+  }
+
+  isAltBackground() {
+    return this.settingsService.isAltBackground();
   }
 }
